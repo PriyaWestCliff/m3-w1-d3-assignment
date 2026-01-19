a2d675694f5be64b24929cd214dbbfffd2a346eb
PriyaWestCliff <p.poojary8596@westcliff.edu>
2026-01-18 20:53:15 -0800

    Add dotenv config, connect MongoDB, and create Registration model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const router = express.Router();
-
+const mongoose = require('mongoose');
 const { check, validationResult } = require('express-validator');
 
-router.get('/', function(req, res) {
+const router = express.Router();
+
+const Registration = mongoose.model('Registration');
+
+router.get('/', function (req, res) {
   res.render('form', { title: 'Registration form' });
 });
 
@@ -16,23 +19,43 @@ router.post(
 
     check('email')
       .isLength({ min: 1 })
-      .withMessage('Please enter an email')
+      .withMessage('Please enter an email'),
   ],
-  function(req, res) {
+  function (req, res) {
     console.log(req.body);
 
     const errors = validationResult(req);
 
     if (errors.isEmpty()) {
-      res.send('Thank you for your registration!');
+      const registration = new Registration(req.body);
+
+      registration
+        .save()
+        .then(() => {
+          res.send('Thank you for your registration!');
+        })
+        .catch((err) => {
+          console.log(err);
+          res.send('Sorry! Something went wrong.');
+        });
     } else {
       res.render('form', {
         title: 'Registration form',
         errors: errors.array(),
-        data: req.body
+        data: req.body,
       });
     }
   }
 );
+
+router.get('/registrations', (req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render('index', { title: 'Listing registrations', registrations });
+    })
+    .catch(() => {
+      res.send('Sorry! Something went wrong.');
+    });
+});
 
 module.exports = router;
--- start.js
+++ start.js
@@ -1,5 +1,26 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+/* With mongoose 9.1.4, those two options are not supported anymore so its crashing just updated to below
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});*/
+
+mongoose.connect(process.env.DATABASE);
+
+mongoose.connection
+  .on('open', () => {
+    console.log('Mongoose connection open');
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+require('./models/Registration');
+
 const app = require('./app');
 
-const server = app.listen(3000, function() {
+const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
